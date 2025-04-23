f33751a226acfd460feba9a6aeba9cf4f58f8247
Hugo-r2d2 <ihugocs@gmail.com>
2025-04-23 13:43:28 -0400

    quebrando linha

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ function verificarChute() {
         if (chute > numeroSecreto) {
             exibirTextoNaTela('p', 'O número secreto é menor');
         } else {
-            exibirTextoNaTela('p', 'O número secreto é maior');
+            exibirTextoNaTela('p', 'O número secreto é maior')
+            ;
         }
         tentativas++;
         limparCampo();
